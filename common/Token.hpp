16eb9a185f966ca8a203cc7a5ad250838cc48943
Kreap-0 <1493025761@qq.com>
2025-11-18 00:25:29 +0800

    token constants

---
+++ common/Token.hpp
@@ -0,0 +1,84 @@
+#pragma once
+
+#include <string>
+#include <unordered_map>
+
+enum TokenType {
+    NONE,
+    BEGIN,
+    END,
+    INTEGER,
+    IF,
+    THEN,
+    ELSE,
+    FUNCTION,
+    READ,
+    WRITE,
+    ID,
+    CONSTANT,
+    EQ,
+    NEQ,
+    LEQ,
+    LE,
+    GEQ,
+    GE,
+    MINUS,
+    MUL,
+    ASSIGN,
+    LB,
+    RB,
+    SEMICOLON
+};
+
+static constexpr std::string keywordLiteral[] = {
+    "",
+    "begin", "end", "integer",
+    "if", "then", "else",
+    "function", "read", "write",
+    "", "", "=",
+    "<>", "<=", "<",
+    ">=", ">", "-",
+    "*", ":=", "(",
+    ")", ";"
+};
+
+static const std::unordered_map<std::string, TokenType> stringToTokenMap = {
+    {"begin", BEGIN},
+    {"end", END},
+    {"integer", INTEGER},
+    {"if", IF},
+    {"then", THEN},
+    {"else", ELSE},
+    {"function", FUNCTION},
+    {"read", READ},
+    {"write", WRITE},
+    {"=", EQ},
+    {"<>", NEQ},
+    {"<=", LEQ},
+    {"<", LE},
+    {">=", GEQ},
+    {">", GE},
+    {"-", MINUS},
+    {"*", MUL},
+    {":=", ASSIGN},
+    {"(", LB},
+    {")", RB},
+    {";", SEMICOLON}
+};
+
+static inline TokenType getTokenType(const std::string& str) {
+    auto it = stringToTokenMap.find(str);
+    if (it != stringToTokenMap.end()) {
+        return it->second;
+    }
+    return NONE;
+}
+
+class Token {
+public:
+    TokenType type;
+    const std::string word;
+
+    Token() = default;
+    Token(const TokenType type, const std::string& word) : type(type), word(word) {}
+};
