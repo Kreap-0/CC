871b7add4752ae2728f9686a4891d64268f139c6
Kreap-0 <1493025761@qq.com>
2025-11-18 14:44:41 +0800

    standardize IO

--- lexer/src/main.cpp
+++ lexer/src/main.cpp
@@ -1,15 +1,24 @@
 #include <iostream>
+#include <fstream>
 
 #include <Utils.hpp>
 #include <Lexer.hpp>
 
-int main() {
-    auto str = read_file_to_string("tmp.txt");
+int main(int argc, char* argv[]) {
+
+    std::ofstream ofserr("lexer.err"), ofsdyd("lexer.dyd");
+
+    if (argc != 2 || !std::string(argv[1]).ends_with(".txt")) {
+        ofserr << "Command line argument error" << std::endl;
+        return 0;
+    }
+
+    auto str = read_file_to_string(argv[1]);
     
     Lexer lexer;
     auto tokens = lexer.tokenize(str);
 
     for (const Token& token : tokens) {
-        std::cout << token.lexeme << " " << token.type << std::endl;
+        ofsdyd << token.lexeme << " " << token.type << std::endl;
     }
 }
